1ea110df7d0b5290f52af81f544399747f8446c3
Rogério da Silva <rogerio410@192.168.0.16>
2025-02-04 10:23:13 -0300

    Font updated fix

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,6 +1,9 @@
 import {Inter, Lusitana} from 'next/font/google';
 
 export const inter = Inter({subsets: ['latin']});
-export const lusitana = Lusitana({weight: ['400', '700']});
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+});
 // export const lusitana_700 = Lusitana({weight: '700'});
 
